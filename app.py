b0b8fa3c5a5c883095c9cad6d9519b75ce8d98a8
Yassir Boulaamane <7014404+yboulaamane@users.noreply.github.com>
2024-02-17 10:22:28 +0100

    Model update

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 from rdkit import Chem
-from rdkit.Chem import AllChem
+from rdkit.Chem import AllChem, MACCSkeys
+from mordred import Calculator, descriptors
+from rdkit.Avalon.pyAvalonTools import GetAvalonFP
 import base64
 import pickle
 
@@ -15,12 +17,19 @@ def calculate_fingerprints(smiles, fingerprint_type):
         if mol is not None:
             if fingerprint_type == 'Morgan fingerprints':
                 fingerprint = AllChem.GetMorganFingerprintAsBitVect(mol, 2, nBits=2048)
+                fingerprint = list(fingerprint.ToBitString())
             elif fingerprint_type == 'MACCS fingerprints':
-                fingerprint = AllChem.GetMACCSKeysFingerprint(mol)
+                fingerprint = MACCSkeys.GenMACCSKeys(mol)
+                fingerprint = list(fingerprint.ToBitString())
+            elif fingerprint_type == 'Avalon fingerprints':
+                fingerprint = GetAvalonFP(mol, nBits=2048)      
+                fingerprint = list(fingerprint.ToBitString())
+            elif fingerprint_type == 'Topological Torsion fingerprints':
+                fingerprint = AllChem.GetHashedTopologicalTorsionFingerprintAsBitVect(mol, nBits=2048)    
+                fingerprint = list(fingerprint.ToBitString())               
             else:
                 st.error("Invalid fingerprint type selected.")
                 return
-            fingerprint = list(fingerprint.ToBitString())
         else:
             fingerprint = [None] * 2048  # Placeholder for invalid molecules
         fingerprints.append(fingerprint)
@@ -31,7 +40,7 @@ def calculate_fingerprints(smiles, fingerprint_type):
 def filedownload(df):
     csv = df.to_csv(index=False)
     b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="prediction.csv">Download Predictions</a>'
+    href = f'<a href="data:file/csv;base64,{b64}" download="ensemblebbb_prediction_results.csv">Download Predictions</a>'
     return href
 
 # Model prediction
@@ -48,7 +57,7 @@ st.image(image, use_column_width=True)
 st.markdown("""
 # BBB Permeability Prediction App
 
-This app allows you to predict the blood-brain barrier permeability of drugs based on Morgan or MACCS fingerprints. The ensemble models were built using four classifiers on the B3DB dataset.
+This app allows you to predict the blood-brain barrier permeability of drugs based on Morgan (ECFP4), MACCS, Avalon or Topological Torsion fingerprints. The ensemble models were built using four machine learning classifiers (RF, SVC, kNN, and XGB) using the [B3DB dataset](https://www.nature.com/articles/s41597-021-01069-5) with 7996 molecules.
 
 **Credits**
 - App built in `Python` + `Streamlit` by Yassir Boulaamane.
@@ -60,18 +69,26 @@ Boulaamane, Y., & Maurady, A. (2023). EnsembleBBB: Enhanced accuracy in predicti
 ---
 """)
 
-
-
 # Sidebar
 with st.sidebar.header('1. Upload your CSV data'):
     uploaded_file = st.sidebar.file_uploader("Upload your input file", type=['csv'])
     st.sidebar.markdown("""
 [Example input file](https://raw.githubusercontent.com/yboulaamane/EnsembleBBB/main/example.csv)
 """)
-    fingerprint_type = st.sidebar.selectbox('2. Choose fingerprint type', ['Morgan fingerprints', 'MACCS fingerprints'])
 
 # Adding an input box for smiles
-smiles_input = st.sidebar.text_area("Enter smiles of molecules (one per line)", height=200)
+smiles_input = st.sidebar.text_area("Enter list of SMILES of molecules (one per line)", height=200)
+
+st.sidebar.markdown("""
+Example:
+""")
+st.sidebar.markdown("""
+CN1C=NC2=C1C(=O)N(C(=O)N2C)C
+""")
+
+st.sidebar.header('2. Choose fingerprint type')
+fingerprint_type = st.sidebar.selectbox('Choose fingerprint type', ['Morgan fingerprints', 'MACCS fingerprints', 'Avalon fingerprints', 'Topological Torsion fingerprints'])
+
 
 if st.sidebar.button('Predict'):
     # Check if either the file or smiles_input is provided
@@ -90,10 +107,32 @@ if st.sidebar.button('Predict'):
 
         st.header('**Original input data**')
         st.write(load_data)
+        
+        # Read descriptor list based on the selected fingerprint type
+        if fingerprint_type == 'Morgan fingerprints':
+            descriptor_list = pd.read_csv('b3db_morgan_fp.csv')
+        elif fingerprint_type == 'MACCS fingerprints':
+            descriptor_list = pd.read_csv('b3db_maccs_fp.csv')
+        elif fingerprint_type == 'Avalon fingerprints':
+            descriptor_list = pd.read_csv('b3db_avalon_fp.csv')
+        elif fingerprint_type == 'Topological Torsion fingerprints':
+            descriptor_list = pd.read_csv('b3db_topological_torsion_fp.csv')
+        else:
+            st.error("Invalid fingerprint type selected.")
+            st.stop()
+
+        # Display the DataFrame of the descriptor lists
+        st.subheader(f'Descriptor List for {fingerprint_type}')
+        st.write(descriptor_list)
+        st.write("Shape of Descriptors:")
+        st.write(descriptor_list.shape)        
+
 
         # Calculate descriptors for the loaded data
         with st.spinner("Calculating descriptors..."):
+        
             features = calculate_fingerprints(load_data['Smiles'], fingerprint_type)
+                
 
         # Convert features to DataFrame
         if fingerprint_type == 'Morgan fingerprints':
@@ -102,32 +141,60 @@ if st.sidebar.button('Predict'):
         elif fingerprint_type == 'MACCS fingerprints':
             num_bits = 167
             fingerprint_type_name = 'MACCS'
+        elif fingerprint_type == 'Avalon fingerprints':
+            num_bits = 2048
+            fingerprint_type_name = 'Avalon'
+        elif fingerprint_type == 'Topological Torsion fingerprints':
+            num_bits = 2048
+            fingerprint_type_name = 'TopologicalTorsion'
         else:
             st.error("Invalid fingerprint type selected.")
             st.stop()
 
         # Create feature names consistent with the model's expectations
-        feature_names = [f'{fingerprint_type_name}FP_{i}' for i in range(num_bits)]
+        
+        feature_names = [f'{fingerprint_type_name}_{i}' for i in range(num_bits)]
+        
 
         # Create DataFrame outside the if-else block
         features_df = pd.DataFrame(features, columns=feature_names)
 
+        # Apply trained model to make prediction on query compounds
+        Xlist = list(descriptor_list.columns)  # Store feature names for subsetting
+
+        
+        # Select subset of descriptors based on the chosen fingerprint type
+        st.subheader(f'Calculating Descriptors for input molecules')
+        desc_subset = features_df[Xlist]
+        # Convert columns to numeric types
+        desc_subset = desc_subset.apply(pd.to_numeric, errors='coerce')
+
+        # Display subset of descriptors and its shape
+        st.write("Subset of Descriptors:")
+        st.write(desc_subset)
+        st.write("Shape of Subset:")
+        st.write(desc_subset.shape)
+        
         # Save descriptors to CSV file
         descriptors_output_file = "descriptors_output.csv"
         features_df.to_csv(descriptors_output_file, index=False)
         st.success(f"Descriptors saved to {descriptors_output_file}")
 
-        # Read descriptor list used in previously built model
+        # Load previously built ensemble model
         if fingerprint_type == 'Morgan fingerprints':
             model = 'ensemble_morgan.pkl'
         elif fingerprint_type == 'MACCS fingerprints':
             model = 'ensemble_maccs.pkl'
+        elif fingerprint_type == 'Avalon fingerprints':
+            model = 'ensemble_avalon.pkl'
+        elif fingerprint_type == 'Topological Torsion fingerprints':
+            model = 'ensemble_topological_torsion.pkl'
         else:
             st.error("Invalid fingerprint type selected.")
             st.stop()
 
         # Apply trained model to make prediction on query compounds
-        prediction = predict_model(features_df, model)
+        prediction = predict_model(desc_subset, model)
         
         # Map numeric predictions to BBB labels
         prediction_labels = ['BBB+' if pred == 1 else 'BBB-' for pred in prediction]
@@ -136,12 +203,6 @@ if st.sidebar.button('Predict'):
         output_df = pd.DataFrame({
             'Smiles': load_data['Smiles'],
             'Prediction': prediction_labels
-        })
-
-        # Prepare output DataFrame
-        output_df = pd.DataFrame({
-            'Smiles': load_data['Smiles'],
-            'BBB Class': prediction_labels
         })
 
         st.header('**Prediction output**')
